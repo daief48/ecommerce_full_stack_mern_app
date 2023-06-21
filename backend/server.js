92fde4cd6b2e01a3e720fe2e4e2a26f8d7f6fdc8
daief48 <daief20103048@gmail.com>
2023-06-21 11:46:29 +0600

    Fix error

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,23 @@ const connectDatabase = require("./config/database");
 //config 
 dotenv.config({path:"backend/config/config.env"});
 
+//Handling Uncaught Exception
+process.on("uncaughtException",(err) =>{
+    console.log(`Error : ${err.message}`);
+    console.log(`Sutting down the server due to Uncaught Exception`)
+    process.exit(1);
+})
 //connecting to database
 connectDatabase();
-app.listen(process.env.PORT,() =>{
+const server = app.listen(process.env.PORT,() =>{
     console.log(`Server is working on http://localhost:${process.env.PORT}`.bgGreen.white);
+})
+
+process.on("unhandledRejection", (err) =>{
+    console.log(`Error: ${err.message}`)
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`)
+
+    server.close(() =>{
+        process.exit(1);
+    })
 })
