56eabb8abab71316c513adc24cb8345458f2fe65
daief48 <daief20103048@gmail.com>
2023-07-11 20:06:06 +0600

    add express server and mongoose server

---
+++ backend/controllers/productController.js
@@ -0,0 +1,3 @@
+exports.getAllProducts = (req, res) => {
+    res.status.json({message: "Route is working fine"});
+}
---
+++ backend/models/productModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please Enter product Name"],
+        trim: true
+    },
+    description:{
+        type: String,
+        required:[true,"Please Enter product Description"]
+    },
+    price:{
+        type: Number,
+        required: [true,"Please Enter product Price"],
+        maxLength: [8,"Price cannot exceed 8 characters"]
+    },
+    rating:{
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true,
+            },
+            url: {
+                type: String,
+                required: true,
+            }
+        }
+    ]
+})
