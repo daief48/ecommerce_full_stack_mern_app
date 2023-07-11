59dc224e3a0be04f89da2d35abb33e427c838e04
daief48 <daief20103048@gmail.com>
2023-07-11 21:53:17 +0600

    add product api

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -30,5 +30,41 @@ const productSchema = mongoose.Schema({
                 required: true,
             }
         }
-    ]
+    ],
+    category:{
+        type: String,
+        required: [true, "Please Enter Product Category"]
+    },
+    Stock:{
+        type: Number,
+        required: [true,"Please Enter product Stock"],
+        maxLength:[4,"Stock cannot exceed 4 characters"],
+        default:1
+    },
+    numOfReviews:{
+        type: Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type: String,
+                required: true,
+            },
+            rating:{
+                type: Number,
+                required: true,
+            },
+            comment:{
+                type: String,
+                required: true,
+            }
+        }
+    ],
+    createAt:{
+        type: Date,
+        default: Date.now
+    }
 })
+
+module.exports = mongoose.model("Product",productSchema);
